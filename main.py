c80dd09c0acaa32f27c7c20c742bba545244f59a
DHN Chandan <php.chandan@gmail.com>
2021-04-23 20:13:22 +0600

    feat: :sparkles: Add about route with dictionary return

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def index():
     return "Hello world!"
+
+
+@app.get("/about")
+def about():
+    return {"data": {"name": "DHN", "age": 35, "email": "php.chandan@gmail.com"}}
