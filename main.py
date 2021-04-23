92ec4c968cb14401cdc18d3d05431932ba19e99b
DHN Chandan <php.chandan@gmail.com>
2021-04-23 20:26:54 +0600

    feat: :sparkles: Add dynamic route

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ def index():
 @app.get("/about")
 def about():
     return {"data": {"name": "DHN", "age": 35, "email": "php.chandan@gmail.com"}}
+
+
+# Dynamic route
+# {id} will be come from url
+@app.get("/blog/{id}")
+def blog(id):
+    return {"data": {"id": id, "title": "Blog id " + id}}
