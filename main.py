4d27bbc8494b4bcc339e7269b81d60a0f77bfd46
DHN Chandan <php.chandan@gmail.com>
2021-04-24 20:35:55 +0600

    fix: :bug: Fix path parameter conflict issue

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ def about():
     return {"data": {"name": "DHN", "age": 35, "email": "php.chandan@gmail.com"}}
 
 
+# If two routes may have same endpoint like below,
+# the static one must be executed earlier
+# Otherwise it will show error
+@app.get("/blog/edit")
+def blog():
+    return {"data": {"title": "This must be executed before path parameter"}}
+
+
 # Dynamic route
 # {id} will be come from url
 @app.get("/blog/{id}")
